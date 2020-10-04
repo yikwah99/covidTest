69e22f0ea0897d6597542999d740e822f9f1693d
cjuan99 <sky.caijuan@gmail.com>
2020-10-04 17:45:45 +0800

    login & register done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,5 +11,32 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onLogin(form: NgForm) {
+    /*alert('Post added')*/
+    /*this.newPost ='The user\'s post';*/
+    // this.newPost = this.enteredValue;
+    /*const post = {
+      title: this.enteredTitle,  //store the data from textbox
+      content: this.enteredContent //store the data from textarea
+    };*/
 
+
+    if(form.invalid){
+      return;
+    }else{
+      console.log("Form Submitted!");
+
+    }
+    /*const post: Post = {
+     // title: this.enteredTitle,  //store the data from textbox
+      //content: this.enteredContent //store the data from textarea
+      title: form.value.title,
+      content: form.value.content
+    };*/
+
+    //to emit a new event pass the post as argument
+    //this.postCreated.emit(post);
+
+    form.resetForm();
+  }
 }
--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
-  Roles: any = ['Patient','Test Centre Manager', 'Test Centre Officer', 'Tester'];
+  Roles: any = ['Patient','Test Centre Manager', 'Tester'];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRegister(form: NgForm) {
+    if (form.invalid) {
+      return;
+    } else {
+      console.log('Form Submitted!');
+    }
+
+    form.resetForm();
   }
 
 }
