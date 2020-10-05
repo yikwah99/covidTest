eda674989a316675e714c77802396292c329074f
kam yik wah <yikwah99@gmail.com>
2020-10-05 20:51:07 +0800

    added-new-components-to-routing.ts

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -5,6 +5,10 @@ import { ManageTestkitComponent } from '../manage-testkit/manage-testkit.compone
 import { TestcentreRegisterComponent } from '../testcentre-register/testcentre-register.component';
 import { UserRegisterComponent } from '../user-register/user-register.component';
 import { LoginComponent } from '../login/login.component';
+import { RecordTestComponent } from '../record-test/record-test.component';
+import { UpdateTestComponent } from '../update-test/update-test.component';
+import { ViewTestComponent } from '../view-test/view-test.component';
+
 
 export const routes: Routes = [
   { path: 'register',     component: UserRegisterComponent },
@@ -13,6 +17,9 @@ export const routes: Routes = [
   /*test manager*/
   { path: 'rCentre',  component: TestcentreRegisterComponent },
   { path: 'manageTestKit',  component: ManageTestkitComponent },
-
+  /*tester/patient*/
+  { path: 'recordTest',  component: RecordTestComponent },
+  { path: 'updateTest',  component: UpdateTestComponent },
+  { path: 'viewTest',  component: ViewTestComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
