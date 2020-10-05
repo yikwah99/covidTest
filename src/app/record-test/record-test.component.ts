a13f02b61cc5a3c3078d521b88b64e0854fe5e62
kam yik wah <yikwah99@gmail.com>
2020-10-05 20:49:10 +0800

    added-tester-patient-file

---
+++ src/app/record-test/record-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-record-test',
+  templateUrl: './record-test.component.html',
+  styleUrls: ['./record-test.component.css']
+})
+export class RecordTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
