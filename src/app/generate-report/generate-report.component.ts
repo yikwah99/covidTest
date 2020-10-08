6ae6db5f05a69f7cb10702bcd79a1869019211ef
kam yik wah <yikwah99@gmail.com>
2020-10-08 20:08:19 +0800

    completed-update-test-result

--- src/app/generate-report/generate-report.component.ts
+++ src/app/generate-report/generate-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {formatDate} from '@angular/common';
 import {MatSort} from '@angular/material/sort';
 import { ViewTestComponent } from '../view-test/view-test.component';
+import { UpdateTestResultComponent } from '../update-test-result/update-test-result.component';
 import { MatTableDataSource } from '@angular/material/table';
 import {MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 export interface TestReport {
@@ -14,15 +15,15 @@ export interface TestReport {
 }
 
 const ELEMENT_DATA: TestReport[] = [
-  {testID: 'TA001', result: 'Positive',  resultDate: new Date(2020, 8, 1), status: 'Completed',tester:"Abu", patientType:'Completed'},
-  {testID: 'TA002', result: 'Positive',  resultDate: new Date(2020, 9, 2), status: 'Completed',tester:"Marcus",patientType:'Completed'},
-  {testID: 'TA003', result: 'Negative',  resultDate: new Date(2020, 9, 3), status: 'Completed',tester:"Jack",patientType:'Completed'},
-  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Pending'},
+  {testID: 'TA001', result: 'Positive',  resultDate: new Date(2020, 8, 1), status: 'Completed',tester:"Abu", patientType:'Returnee'},
+  {testID: 'TA002', result: 'Positive',  resultDate: new Date(2020, 9, 2), status: 'Completed',tester:"Marcus",patientType:'Returnee'},
+  {testID: 'TA003', result: 'Negative',  resultDate: new Date(2020, 9, 3), status: 'Completed',tester:"Jack",patientType:'Returnee'},
+  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Returnee'},
   
-  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Pending'},
-  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Pending'},
-  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Pending'},
-  {testID: 'TA004', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Pending'},
+  {testID: 'TA005', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Returnee'},
+  {testID: 'TA006', result: '-',  resultDate: null, status: 'Pending',tester:"Mona",patientType:'Returnee'},
+  {testID: 'TA007', result: '-',  resultDate: null, status: 'Pending',tester:"Chong",patientType:'Returnee'},
+  {testID: 'TA008', result: '-',  resultDate: null, status: 'Pending',tester:"Mia",patientType:'Returnee'},
 
 
 
@@ -36,7 +37,7 @@ const ELEMENT_DATA: TestReport[] = [
 
 export class GenerateReportComponent implements OnInit {
 
-  displayedColumns: string[] = ['testID', 'result', 'resultDate', 'status','tester','patientType'];
+  displayedColumns: string[] = ['testID', 'result', 'resultDate', 'status','tester','action'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   constructor(private dialog:MatDialog) {
 
@@ -53,9 +54,17 @@ export class GenerateReportComponent implements OnInit {
     this.dataSource.filter = this.searchKey.trim().toLowerCase();
   }
 
-  onViewReport(){
+  onViewReport(status: string){
     const dialogConfig = new MatDialogConfig();
-    this.dialog.open(ViewTestComponent,dialogConfig);
+    if (status === 'Completed'){
+      console.log("view");
+      this.dialog.open(ViewTestComponent,dialogConfig);
+    }
+    else{
+      console.log("update");
+      this.dialog.open(UpdateTestResultComponent,dialogConfig);
+    }
+    
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.width = "60%";
