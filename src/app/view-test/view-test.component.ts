af81425efbdb71a5121b261f1a0dd8ef0223ac77
yikwah99 <yikwah99@gmail.com>
2020-11-25 07:16:22 +0800

    added hard code test

--- src/app/view-test/view-test.component.ts
+++ src/app/view-test/view-test.component.ts
@@ -4,6 +4,7 @@ import {Test} from '../test.model';
 import {PatientService} from '../record-patient.service';
 import {RecordTestService} from '../record-test.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-view-test',
   templateUrl: './view-test.component.html',
@@ -14,7 +15,9 @@ export class ViewTestComponent implements OnInit {
   username: string;
   id: string;
   test: Test;
+  tests: Test[]=[];
   
+  private testSub:Subscription;
   
   constructor(
   public patientService: PatientService, 
@@ -24,11 +27,23 @@ export class ViewTestComponent implements OnInit {
   }
 
   ngOnInit(){
+    
+    this.patientService.getPatients();
+    this.testService.getTests();
     this.id = this.testService.getID();
     this.username = this.testService.getUsername();
+    if (this.username==""){
+      this.username = "patient";
+      console.log(this.username);
+    }
+    if (this.id ==""){
+      this.id="5fbd8e5a46e3510b2cd7eb3b";
+    }
     this.patient = this.patientService.getPatient(this.username);
+    console.log(this.patient);
     this.test = this.testService.getTest(this.id);
-    
+    console.log(this.test);
   }
 
 }
+
